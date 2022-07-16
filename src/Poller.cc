e1c6e5da10fbb01ef6f35adb5a5a036f8933fe6c
sunnangeo <396243066@qq.com>
2022-07-16 14:02:20 +0800

    first commit

---
+++ src/Poller.cc
@@ -0,0 +1,15 @@
+#include "../include/Poller.h"
+#include "../include/Channel.h"
+
+Poller::Poller(EventLoop *loop)
+    : ownerLoop_(loop)
+{
+}
+
+bool Poller::hasChannel(Channel *channel) const
+{
+    auto it = channels_.find(channel->fd());
+    return it != channels_.end() && it->second == channel;
+}
+
+//静态方法不在poller实现，因为defalt一定会include epoller 这不合理
